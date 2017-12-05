84bb0349072de0bdc5b04e881325b29c928ec11b
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-05 11:14:39 +0600

    nr

--- AWS/env_variables.py
+++ AWS/env_variables.py
@@ -4,5 +4,5 @@ def getVariable(var):
         lines = file.readlines()
         for line in lines:
             line = line.split('=')
-            keys[line[0]] = line[1]
+            keys[line[0]] = line[1].replace('\n','')
     return (keys[var] if(var in keys.keys()) else None)
