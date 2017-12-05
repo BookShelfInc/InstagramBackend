551cd9665adc355cf3bd2d38b876e0df53154446
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-05 10:36:38 +0600

    new

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -81,24 +81,16 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if(getVariable('RDS_HOST')):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': str(getVariable('RDS_NAME')),
-            'USER': str(getVariable('RDS_USER')),
-            'PASSWORD': str(getVariable('RDS_PASSWORD')),
-            'HOST': str(getVariable('RDS_HOST')),
-            'PORT': str(getVariable('RDS_PORT')),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': str(getVariable('RDS_NAME')),
+        'USER': str(getVariable('RDS_USER')),
+        'PASSWORD': str(getVariable('RDS_PASSWORD')),
+        'HOST': str(getVariable('RDS_HOST')),
+        'PORT': str(getVariable('RDS_PORT')),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+}
 
 
 # Password validation
