678df75344867a47b96a0324d522d6078c7fb46d
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-06 19:48:58 +0600

    new

--- photo_app/admin.py
+++ photo_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Photo, Comment, Like
+from .models import Photo, Comment, Like, Bookmark
 
 admin.site.register(Photo)
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(Bookmark)
