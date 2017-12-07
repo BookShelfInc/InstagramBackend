866319aadcb6f591a0898bc8bc250000fa6949ee
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-07 15:22:10 +0600

    new

--- AWS/api_gateway.py
+++ AWS/api_gateway.py
@@ -7,7 +7,7 @@ def cropImage(image):
     API_ENDPOINT = getVariable('API_GATEWAY_IMAGE')
 
     data = {'bucket': getVariable('s3BucketName'), 'image': image}
-    headers = {'Content-type': 'application/json'}
+    headers = {'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 
     r = requests.post(API_ENDPOINT, data=json.dumps(data), headers=headers)
 
