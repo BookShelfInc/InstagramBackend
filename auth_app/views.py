7408e2733a3008e0ae122e0d7e12e54bf574260b
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-08 10:04:12 +0600

    neW

--- auth_app/views.py
+++ auth_app/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.parsers import JSONParser
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
+from .models import User
 from .serializers import UserSerializer, UserPhotoSerializer
 
 def register(request):
@@ -26,4 +27,14 @@ def getInfo(request):
         user = request.user
         serialized = UserPhotoSerializer(user)
         return JsonResponse(serialized.data, status=status.HTTP_200_OK)
+    return HttpResponse(status=400)
+
+@api_view(['GET'])
+@authentication_classes([JSONWebTokenAuthentication, ])
+@permission_classes([IsAuthenticated, ])
+def getAllUsers(request):
+    if(request.method == 'GET'):
+        allUsers = User.objects.all()
+        serialized = UserPhotoSerializer(allUsers, many=True)
+        return JsonResponse(serialized.data, status=status.HTTP_200_OK, safe=False)
     return HttpResponse(status=400)
